ad1de21e8691372d5c46f4d4da412c6a3cdf5875
WiiLin <ya790504@gmail.com>
2019-05-02 14:56:42 +0800

    set pod lib create

---
+++ Example/MochiPay/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MochiPay
+//
+//  Created by ya790504@gmail.com on 05/02/2019.
+//  Copyright (c) 2019 ya790504@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
